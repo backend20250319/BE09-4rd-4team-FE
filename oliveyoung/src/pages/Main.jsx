8e35b38a5057cbf56fe3f39cda75c6e6bf31117e
dkrio <dkrio34@gmail.com>
2025-06-25 17:46:00 +0900

    리뷰 0625 1차 작업

--- oliveyoung/src/pages/Main.jsx
+++ oliveyoung/src/pages/Main.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
-
+import ReviewWriteSection from '../review/pages/ReviewWriteSection';
+import Reviewtitle from '../review/pages/Reviewtitle';
 function Main() {
-  
+  return (
+    <div>
+      <div className="max-w-4xl mx-auto py-10">
+        <ReviewWriteSection />
+      </div>
+      <div className="max-w-4xl mx-auto py-10">
+        <Reviewtitle />
+      </div>
+    </div>
+  );
 }
+
 
 export default Main;
---
+++ oliveyoung/src/review/pages/ReviewTabs.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function ReviewTabs() {
+  const [activeTab, setActiveTab] = useState("write");
+
+  return (
+    <div className="w-[785.6px] mx-auto mt-4">
+      {/* 탭 영역 */}
+      <div className="flex">
+        {/* 리뷰 작성 탭 */}
+        <button
+          className={`w-1/2 h-[50px] text-[18px] font-medium rounded-t-md bg-white ${
+            activeTab === "write"
+              ? "border-t-2 border-l-2 border-r-2 border-[#9bce26] border-b-0 text-black z-10"
+              : "bg-gray-100 border-b-2 border-[#9bce26] border-t-0 border-l-0 border-r-0 text-black"
+          }`}
+          onClick={() => setActiveTab("write")}
+        >
+          리뷰 작성
+        </button>
+
+        {/* 나의 리뷰 탭 */}
+        <button
+          className={`w-1/2 h-[50px] text-[18px] font-medium rounded-t-md ${
+            activeTab === "mine"
+              ? "border-t-2 border-l-2 border-r-2 border-[#9bce26] border-b-0 bg-white text-black z-10"
+              : "bg-gray-100 border-b-2 border-[#9bce26] border-t-0 border-l-0 border-r-0 text-black"
+          }`}
+          onClick={() => setActiveTab("mine")}
+        >
+          나의 리뷰
+        </button>
+      </div>
+    </div>
+  );
+}
