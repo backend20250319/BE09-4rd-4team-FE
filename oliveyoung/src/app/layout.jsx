83f956681ecbdcad16d9046264c0c8d76d64f139
parkchangjun <parkchangjun13@gmail.com>
2025-06-27 12:11:50 +0900

    header, footer, menu update

---
+++ oliveyoung/src/app/layout.jsx
@@ -0,0 +1,20 @@
+// src/app/layout.jsx
+import Header from '../components/Header'; // 헤더 임포트
+import Footer from '../components/Footer'; // 푸터 임포트
+import Menu from '../app/menu/Menu';
+import '../styles/globals.css'; // 글로벌 CSS 임포트
+
+export default function Layout({ children }) {
+  return (
+    <html lang="ko">
+      <head />
+      <body>
+        {/* 공통 레이아웃 구성: Header, Main Content (children), Footer */}
+        <Header />
+        <Menu />
+        <main>{children}</main> {/* 각 페이지의 내용이 여기에 들어감 */}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ oliveyoung/src/app/menu/RecentSearch.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function RecentSearch({ search }) {
+  return (
+    <div className="w-[100%] py-[9px] flex flex-row justify-between items-center">
+      <p className="text-sm text-[#131518]">스킨/토너</p>
+      <div>
+        <img src="/images/delete.svg" alt="delete" /> {/* 절대 경로로 수정 */}
+      </div>
+    </div>
+  );
+}
+export default RecentSearch;
