78515f83c48a9457742024aa89e6f2a14f4a2b8e
parkchangjun <parkchangjun13@gmail.com>
2025-06-27 17:51:31 +0900

    product upload

--- oliveyoung/src/app/product/layout.jsx
+++ oliveyoung/src/app/product/layout.jsx
@@ -1,6 +1,4 @@
 // 예: src/app/layout.jsx
-import Header from '../../components/Header'; // 절대/상대경로로 조정
-import Footer from '../../components/Footer';
 import '../../styles/globals.css'; // tailwind 등 공통 스타일
 
 export default function Layout({ children }) {
---
+++ oliveyoung/src/app/product/skintoner/page.jsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>스킨/토너 페이지</h1>
+    </div>
+  );
+}
