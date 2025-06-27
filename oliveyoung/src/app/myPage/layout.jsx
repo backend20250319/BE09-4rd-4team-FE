97d3440bc6c3bebc017def7a37af2e5d050971b0
Berry-mas <junseo9145@gmail.com>
2025-06-27 12:25:21 +0900

    myPage files upload

---
+++ oliveyoung/src/app/myPage/layout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import MyPageNavBar from "../myPage/MyPageNavBar";
+
+export default function LayoutMyPage({ children }) {
+  return (
+    <div className="flex">
+      {/* 왼쪽 고정 NavBar */}
+      <MyPageNavBar />
+
+      {/* 오른쪽 페이지 바디 영역 */}
+      <div className="flex-1 p-4">
+        {children}
+      </div>
+    </div>
+  );
+}
